3906b0a6b3a80f3196d44577d2745f59920d84fd
zavocc <zavocc1030@outlook.com>
2024-10-23 20:06:38 +0800

    Material UI

---
+++ src/app/theme.js
@@ -0,0 +1,9 @@
+"use client";
+import { createTheme } from '@mui/material/styles';
+// Create a theme
+const default_theme = createTheme({
+    palette: {
+      mode: 'dark'
+    }
+});
+export default default_theme;
